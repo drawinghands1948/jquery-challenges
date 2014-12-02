0a819f160edd3e8bdb0af7d6ea2b9328aae93dfd
drawinghands1948 <jchong87@gmail.com>
2014-12-02 04:52:20 -0500

    sticky

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// mobile nav
 $('#btn-menu').click(function(){
 	$('.nav-main').slideToggle();
 });
+
+//panels
+$('.panel-content').hide();
+$('.panel-title').click(function(){
+	//$('.panel-content').slideToggle();
+	$(this).next().slideToggle();
+});
+
+//sticky
+$(document).ready(function(){
+    $('.nav-internal').sticky({topSpacing:0});
+  });
